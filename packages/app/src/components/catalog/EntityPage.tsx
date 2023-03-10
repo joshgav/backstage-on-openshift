4a405fb4f77cf11f2afb8faf22ced4ede66890c3
Josh Gavant <joshgavant@gmail.com>
2023-03-09 18:57:11 -0600

    add K8s plugin
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -56,6 +56,9 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 
+// import Kubernetes plugin
+import { EntityKubernetesContent } from '@backstage/plugin-kubernetes';
+
 const techdocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
@@ -164,6 +167,10 @@ const serviceEntityPage = (
 
     <EntityLayout.Route path="/docs" title="Docs">
       {techdocsContent}
+    </EntityLayout.Route>
+
+    <EntityLayout.Route path="/kubernetes" title="Kubernetes">
+      <EntityKubernetesContent refreshIntervalMs={30000} />
     </EntityLayout.Route>
   </EntityLayout>
 );
---
+++ scripts/init.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../../.. && pwd)
+if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
+if [[ -e "${this_dir}/.env" ]]; then source ${this_dir}/.env; fi
+source ${root_dir}/lib/kubernetes.sh
+
+export bs_app_name=${1:-${BS_APP_NAME:-bs1}}
+export quay_user_name=${2:-${QUAY_USER_NAME:-${USER}}}
+
+# nvm use --latest --lts
+
+yarn add --cwd packages/app @backstage/plugin-kubernetes
+yarn add --cwd packages/backend @backstage/plugin-kubernetes-backend
