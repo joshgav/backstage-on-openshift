0d364f6b9d570708d68863d8b1f796ad9edb9336
Josh Gavant <4421720+joshgav@users.noreply.github.com>
2023-09-09 16:07:24 -0500

    upgrade to 1.17 (#4)

---
+++ deploy/upgrade.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+# see https://github.com/backstage/backstage/tree/master/contrib/scripts/upgrade-backstage-app
+
+curl https://raw.githubusercontent.com/backstage/backstage/master/contrib/scripts/upgrade-backstage-app/upgrade-backstage-app.sh | sh
